0e542f22096c4f7c75e0de7ab0181a0fe232e773
Alex Kuimov <spoot@bk.ru>
2021-04-02 13:42:20 +0300

    add 3 module all tasks

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,12 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i > 0) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    }
+  }
+
+  str = arr.join('');
+  return str;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let val = str.replace(/[\,%]/g, ' ');
+  let arr = val.split(' ');
+
+  arr = arr.map(Number);
+  arr = arr.filter(item => typeof item === "number" && !isNaN(item));
+
+  let res = {
+    min: Math.min.apply(null, arr),
+    max: Math.max.apply(null, arr),
+  };
+
+  return res;
 }
