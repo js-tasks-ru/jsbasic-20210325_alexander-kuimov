da622e794951c42f7f52901214a39f631318fc0b
Alex Kuimov <spoot@bk.ru>
2021-04-09 20:52:46 +0300

    add 5 module 2 task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let txt = document.querySelector('#text');
+  
+  btn.addEventListener('click', function(event) {
+    if (txt.hasAttribute('hidden')) {
+      txt.removeAttribute('hidden');
+    } else {
+      txt.setAttribute('hidden', '');
+    }
+  });
+
 }
