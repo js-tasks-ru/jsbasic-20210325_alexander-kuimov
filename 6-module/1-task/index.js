d81b3f4c8157eefba07e9493000ec1a8aa80d101
Alex Kuimov <spoot@bk.ru>
2021-04-18 11:04:50 +0300

    add 6 module 3 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,73 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = rows;
   }
+
+  removeListener(table) {
+    table.addEventListener('click', function(event) {
+      let target = event.target;
+      if (target.tagName == 'BUTTON') {
+        let td = target.parentElement;  
+        td.parentNode.remove();
+      }
+    });
+  }
+
+  set elem(rows) {
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let tbody = document.createElement('tbody');
+
+    let head = document.createElement('tr');
+    let name = document.createElement('td');
+    let age = document.createElement('td');
+    let salary = document.createElement('td');
+    let city = document.createElement('td');
+    let action = document.createElement('th');
+
+    name.innerText = 'Имя';
+    age.innerText = 'Возраст';
+    salary.innerText = 'Зарплата';
+    city.innerText = 'Город';
+
+    head.appendChild(name);
+    head.appendChild(age);
+    head.appendChild(salary);
+    head.appendChild(city);
+    head.appendChild(action);
+
+    thead.appendChild(head);
+   
+    for (let item in rows) {
+      let tr = document.createElement('tr');
+      let btn = document.createElement('button'); 
+      let act = document.createElement('td');
+
+      let obj = rows[item];
+      
+      for (let key in obj) {
+        let td = document.createElement('td');
+        td.innerText = obj[key];
+        tr.appendChild(td);
+      }
+      
+      btn.innerText = 'X';
+      act.appendChild(btn);
+      tr.appendChild(act);
+
+      tbody.appendChild(tr);
+    }
+  
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    this._elem = table;
+    this.removeListener(table);
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -4,4 +4,148 @@ export default class Carousel {
   constructor(slides) {
     this.slides = slides;
   }
+
+  addToCard(html) {
+    html.addEventListener('click', function(e) {
+      let target = e.target;
+
+      if (target.tagName == 'BUTTON') {
+        let button = target;       
+        let productId = button.getAttribute('data-product-id');
+
+        let event = new CustomEvent('product-add', {
+          detail: productId, 
+          bubbles: true
+        });
+
+        button.dispatchEvent(event);
+      }
+    });
+  }
+
+  initCarousel(carousel) {
+    const slide = carousel.querySelectorAll('.carousel__slide');
+    const wrapper = carousel.querySelector('.carousel__inner');
+    const prevBtn = carousel.querySelector('.carousel__arrow_left');
+    const nextBtn = carousel.querySelector('.carousel__arrow_right');
+    
+    let step = 500;
+    let counter = 1;
+    let shift = 0;
+
+    prevBtn.style.display = 'none';
+
+    prevBtn.addEventListener('click', function(event) {
+      counter--;
+      shift += step;
+
+      wrapper.style.transform = 'translateX(' + shift + 'px)';
+      
+      nextBtn.style.display = 'flex';
+
+      if (counter === 1) {
+        prevBtn.style.display = 'none';
+      }
+
+    });
+
+    nextBtn.addEventListener('click', function(event) {
+      counter++;
+      shift -= step;
+      wrapper.style.transform = 'translateX(' + shift + 'px)';
+
+      prevBtn.style.display = 'flex';
+
+      if (slide.length === counter) {
+        nextBtn.style.display = 'none';
+      }
+
+    });
+  }
+
+  getWrapHtml(slides) {
+    const wrapper = document.createElement('div');
+    const arrowRight = document.createElement('div');
+    const arrowLeft = document.createElement('div');
+    const iconRight = document.createElement('img');
+    const iconLeft = document.createElement('img');
+
+    wrapper.classList.add('carousel'); 
+    arrowRight.classList.add('carousel__arrow', 'carousel__arrow_right'); 
+    arrowLeft.classList.add('carousel__arrow', 'carousel__arrow_left'); 
+    
+    iconRight.setAttribute('src', '/assets/images/icons/angle-icon.svg');
+    iconLeft.setAttribute('src', '/assets/images/icons/angle-left-icon.svg');
+
+    iconRight.setAttribute('alt', 'icon');
+    iconLeft.setAttribute('alt', 'icon');
+
+    arrowRight.appendChild(iconRight);
+    arrowLeft.appendChild(iconLeft);
+
+    wrapper.appendChild(arrowRight);
+    wrapper.appendChild(arrowLeft);   
+    wrapper.appendChild(slides);
+
+    return wrapper;
+  }
+
+  getSlidersHtml(slides) {
+    const inner = document.createElement('div');
+    inner.classList.add('carousel__inner');
+
+    for (const key in slides) {
+      const slide = document.createElement('div');
+      const image = document.createElement('img');
+      const caption = document.createElement('div');
+      const price = document.createElement('span');
+      const title = document.createElement('div');
+      const button = document.createElement('button');
+      const icon = document.createElement('img');
+
+      slide.classList.add('carousel__slide');
+      image.classList.add('carousel__img');
+      caption.classList.add('carousel__caption');
+      price.classList.add('carousel__price');
+      title.classList.add('carousel__title');
+      button.classList.add('carousel__button');
+
+      slide.setAttribute('data-id', slides[key].id);
+      button.setAttribute('data-product-id', slides[key].id);
+
+      image.setAttribute('alt', 'slide');
+      icon.setAttribute('alt', 'icon');
+      image.setAttribute('src', '/assets/images/carousel/' + slides[key].image);
+      icon.setAttribute('src', '/assets/images/icons/plus-icon.svg');
+
+      price.innerHTML = '€' + slides[key].price.toFixed(2);
+      title.innerHTML = slides[key].name;
+
+      slide.appendChild(image);
+      button.appendChild(icon);
+      caption.appendChild(price);
+      caption.appendChild(title);
+      caption.appendChild(button);
+      slide.appendChild(caption);
+
+      inner.appendChild(slide);
+    }
+
+    return inner;
+  }
+
+  set slides(slides) {
+    const slidesHtml = this.getSlidersHtml(slides);
+    const wrapperHtml = this.getWrapHtml(slidesHtml);
+
+    this.initCarousel(wrapperHtml);
+    this.addToCard(wrapperHtml);
+
+    this._elem = wrapperHtml;
+  }
+
+  get elem() {
+    return this._elem;
+  }
+
 }
